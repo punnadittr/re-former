3c045a99a39ba3f60bf7785a61a75aea0d453bc9
Punnadit Trussat <punnadittr@gmail.com>
2018-06-27 12:18:19 +0700

    finished re-former

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    #@user = User.new(username: params[:username], password: params[:password], email: params[:email])
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password, :email)
+  end
+end
